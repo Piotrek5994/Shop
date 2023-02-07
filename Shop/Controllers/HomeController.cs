99b4784542e574e2a578db8ad4be1c4dfa7c3c6c
Piotrek5994 <piotrek5994@gmail.com>
2023-02-07 19:44:13 +0100

    Add Categories

--- Shop/Controllers/HomeController.cs
+++ Shop/Controllers/HomeController.cs
@@ -12,7 +12,6 @@ namespace Shop.Controllers
         {
             _logger = logger;
         }
-
         public IActionResult Index()
         {
             return View();
---
+++ Shop/Infrastructure/Components/CategoriesViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shop.Infrastructure.Components
+{
+    public class CategoriesViewComponent:ViewComponent
+    {
+
+        private readonly DataContext _context;
+
+        public CategoriesViewComponent(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+
+
+    }
+}
