8b94f10f8b00adbdd4e05207a044a478789c6f9f
Piotrek5994 <piotrek5994@gmail.com>
2023-02-07 17:54:54 +0100

    Try to saw Categories

---
+++ Shop/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Infrastructure;
+
+namespace Shop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ProductsController(DataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
