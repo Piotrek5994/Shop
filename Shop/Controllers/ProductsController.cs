470ab93fb46b8dede0e8ad3640a4930447abae05
Piotrek5994 <piotrek5994@gmail.com>
2023-02-08 16:30:25 +0100

    adding equipment

--- Shop/Controllers/ProductsController.cs
+++ Shop/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Shop.Infrastructure;
+using Shop.Models;
 
 namespace Shop.Controllers
 {
@@ -12,19 +13,27 @@ namespace Shop.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> Index(string categorySlug = "",int p=1)
+        public async Task<IActionResult> Index(string categorySlug = "", int p = 1)
         {
             int pageSize = 6;
             ViewBag.PageNumber = p;
             ViewBag.PageRange = pageSize;
             ViewBag.PageCount = categorySlug;
-            if(categorySlug == "")
+            if (categorySlug == "")
             {
                 ViewBag.TotalPages = (int)Math.Ceiling((double)_context.Products.Count() / pageSize);
 
                 return View(await _context.Products.OrderByDescending(x => x.Id).Skip((p - 1) * pageSize).Take(pageSize).ToListAsync());
             }
-            return View();
+
+            Category category = await _context.Categories.Where(c => c.Slug == categorySlug).FirstOrDefaultAsync();
+            if (category == null)
+                return RedirectToAction("Index");
+
+            var productsByCategory = _context.Products.Where(p => p.CategoryId == category.Id);
+            ViewBag.TotalPages = (int)Math.Ceiling((double)productsByCategory.Count() / pageSize);
+
+            return View(await productsByCategory.OrderByDescending(x => x.Id).Skip((p - 1) * pageSize).Take(pageSize).ToListAsync());
         }
     }
 }
