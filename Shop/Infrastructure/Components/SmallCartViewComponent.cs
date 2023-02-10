5b79a759eceb25316fa3072d90c8e3263a9c85e5
Piotrek5994 <piotrek5994@gmail.com>
2023-02-10 18:24:46 +0100

    Created SmallCartViewComponet to see all prizes order

---
+++ Shop/Infrastructure/Components/SmallCartViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Models;
+using Shop.Models.ViewModels;
+
+namespace Shop.Infrastructure.Components
+{
+    public class SmallCartViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("cart");
+            SmallCartViewModel smallCartVM;
+
+            if (cart == null || cart.Count == 0)
+            {
+                smallCartVM = null;
+            }
+            else
+            {
+                smallCartVM = new ()
+                {
+                    NumberOfitems = cart.Sum(x=> x.Quantity),
+                    TotalAmount = cart.Sum(x => x.Quantity * x.Price)
+                };
+            }
+
+            return View(smallCartVM);
+
+        }
+    }
+}
