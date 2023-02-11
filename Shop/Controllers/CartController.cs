ea3b8c1e78ae6c25c5c42df0da5b9c813c3a61f2
Piotrek5994 <piotrek5994@gmail.com>
2023-02-11 13:12:02 +0100

    small change view

--- Shop/Controllers/CartController.cs
+++ Shop/Controllers/CartController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Infrastructure;
+using Shop.Models;
+using Shop.Models.ViewModels;
 
 namespace Shop.Controllers
 {
     public class CartController : Controller
     {
+        private readonly DataContext _context;
+
+        public CartController(DataContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("cart") ?? new List<CartItem>();
+
+               CartViewModel cartVM = new()
+            {
+                CartItems = cart,
+                GrandTotal = cart.Sum(x => x.Quantity * x.Price)
+            };
+            return View(cartVM);
         }
     }
 }
